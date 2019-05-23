39c1a0a79f9675816549b5327c0a83da7aab7467
IwonaWilk <iwona.k.bialecka@gmail.com>
2019-05-23 17:17:57 +0100

    User model changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
   has_many :group_memberships
   has_many :groups, through: :group_memberships
-  has_many :messages
 end
