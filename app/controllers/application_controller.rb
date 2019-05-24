8b600163297397a94de65f1efd79586b1ff5cc18
Harsh Patel <harshp0301@gmail.com>
2019-05-24 15:32:31 +0100

    Pages and users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,10 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || groups_path
+  end
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
