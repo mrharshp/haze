7ec94e967772e519e80f113c3902e103ac6df8b7
Harsh Patel <harshp0301@gmail.com>
2019-05-24 14:43:58 +0100

    pages 3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,23 @@
 class UsersController < ApplicationController
-  def edit
-    @user = user
+
+  def show
+    @user = current_user
+    authorize @user
   end
 
-  def update
-    @user = user
-    @user.update(user_params)
-    # redirect_to my_profile_path(@user)
-  end
+  # def edit
+  #   @user = user
+  # end
+
+  # def update
+  #   @user = user
+  #   @user.update(user_params)
+  #   # redirect_to my_profile_path(@user)
+  # end
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :photo, :email)
-  end
+  # def user_params
+  #   params.require(:user).permit(:name, :email)
+  # end
 end
