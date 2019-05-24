631b401cdccc46dfa670dc16ab8a6b7c4365ee76
IwonaWilk <iwona.k.bialecka@gmail.com>
2019-05-24 11:17:17 +0100

    Routes and UserController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def edit
+    @user = user
+  end
+
+  def update
+    @user = user
+    @user.update(user_params)
+    # redirect_to my_profile_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :photo, :email)
+  end
 end
