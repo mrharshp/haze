95bf08dd9b9275999a153088f914fc1b368c8ef5
Hussein Daher <hussein.daher.24@gmail.com>
2019-06-03 12:24:31 +0100

    edit update destory done

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,7 @@
 class List < ApplicationRecord
   belongs_to :group
   belongs_to :user
-  has_many :list_items
+  has_many :list_items, dependent: :destroy
   validates :name, presence: true
   mount_uploader :photo, PhotoUploader
 end
